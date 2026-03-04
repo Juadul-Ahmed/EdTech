dbc7a958a63207830cb3c169bb3194caeb114ac6
juadul <parallaxhub@gmail.com>
2026-03-04 15:46:16 +0800

    basic error fixed

--- script/script.js
+++ script/script.js
@@ -15,15 +15,28 @@ const displayLevelWord = (words) => {
   const wordContainer = document.getElementById("word-container");
   wordContainer.innerHTML = "";
 
+  if(words.length == 0){
+    wordContainer.innerHTML = `
+    
+      <div class="text-center col-span-full rounded-xl py-10 space-y-6">
+      <img class="mx-auto" src="assets/alert-error.png" alt="">
+    <p class="text-xl font-medium text-gray-400">Vocabulary is not added to the lesson yet</p>
+    <h2 class="font-bold text-4xl">Try next lesson!</h2>
+  </div>
+    
+    `;
+    return;
+  }
+
   words.forEach((word) => {
     console.log(word);
 
     const card = document.createElement("div");
     card.innerHTML = `
         <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
-    <h2 class="font-bold text-2xl">${word.word}</h2>
+    <h2 class="font-bold text-2xl">${word.word?word.word:"Not available"}</h2>
     <p class="font-semibold">Meaning/Pronunciation</p>
-    <div class="font-semibold font-medium font-bangla">"${word.meaning} / ${word.pronunciation}"</div>
+    <div class="font-semibold font-medium font-bangla">"${word.meaning?word.meaning:"Not available"} / ${word.pronunciation?word.pronunciation:"Not Available"}"</div>
 
     <div class="flex justify-between items-center">
       <button class="btn bg-[#1a91ff10] hover:bg-[#1a91ff80]"><i class="fa-solid fa-circle-info"></i></button>
